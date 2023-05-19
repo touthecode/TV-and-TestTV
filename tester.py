49a82a34b55b80db16343add23bf5e55b73173fa
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-19 23:29:56 +0800

    finished revamp and added functions for test

--- tester.py
+++ tester.py
@@ -1,7 +1,16 @@
-from tv import remote_control
+from tv import Tv
 
+# A function to test the object
 def main():
-    TV = remote_control()
-
-    
-
+    # This function shows the usage of volumeplus and channel plus
+    Tv1 = Tv
+    Tv1.turnOn()
+    Tv1.setChannel(25)
+    Tv1.VolumePlus()
+    Tv1.VolumePlus()
+    Tv1.VolumePlus()
+    Tv1.ChannelPlus()
+    Tv1.ChannelPlus()
+    Tv1.ChannelPlus()
+    Tv1.ChannelPlus()
+    Tv1.ChannelPlus()
