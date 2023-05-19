9ee94876ec2d88a8742a6c7827f335aaf644f579
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-19 19:54:26 +0800

    added constructor for tv

--- tv.py
+++ tv.py
@@ -1,4 +1,8 @@
 # Make the class
 class remote_control():
 
-
+    # Create a parametrized constructor
+    def __init__(self, power = "OFF", volume = 0, channel = 1):
+        self.power = power
+        self.volume = volume
+        self.channel = channel
