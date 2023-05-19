73a5a5cf6274b7900c3a6cf9dc9bd7e8709a5226
touthecode <130022845+touthecode@users.noreply.github.com>
2023-05-19 22:29:14 +0800

    fixed on comment and added off function

--- tv.py
+++ tv.py
@@ -2,11 +2,15 @@
 class Tv():
 
     # Create a constructor
-    def __init__(self, on = "False", volume = 0, channel = 1):
+    def __init__(self, on = False, volume = 0, channel = 1):
         self.on = on
         self.volume = volume
         self.channel = channel
-    # A function for turning the tv off
+    # A function for turning the tv on
     def TvOff(self):
         self.on = True
+    # A function for turning the tv off   
+    def TvOn(self):
+        self.on = False
+    
 
